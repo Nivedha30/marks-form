3f6c68c60be486e4442331ad7a5fa23889140e95
Nivedha30 <nivedhathirumurtho@gmail.com>
2018-03-17 16:16:00 +0530

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  m1: number;
+  m2: number;
+  m3: number;
+  result: number;
+
+  typesOfResult = ['poor', 'average', 'excellent'];
+  currentDate = new Date();
+
+
+  sum(){
+    this.result = (this.m1 ? this.m1 : 0)+
+                     (this.m2 ? this.m2 : 0)   +
+                     (this.m3 ? this.m3 : 0) ;  
+
+  }
+}
+
+
